84fbcaf086bec6d4ed7ea082281494c2ccedaf55
tasioulas221 <cs05490@uoi.gr>
2024-11-16 17:35:44 +0200

    Completed lab05

--- lab05.s
+++ lab05.s
@@ -67,6 +67,9 @@ taken:
 # ----------------------------------------------------------------------------------------
 # TODO: Add an example where an instruction passes its result to the 2nd following instruction
 # There should be no stalls
+    add s5 s3 s2
+    add s4 s1 s2
+    add s3 s5 s2
 # ----------------------------------------------------------------------------------------
     # nop instructions added between examples
     add  zero, zero, zero  
@@ -78,6 +81,10 @@ taken:
 # A double hazzard is when the source register of an instruction matches the destination
 #  registers of both of the two instructions preceeding it. It should get the newest value.
 # There should be no stalls
+
+    add  t1, s0, s1   
+    add t1, s0, s2   
+    add  t3, t1, s3  
 # ----------------------------------------------------------------------------------------
     # nop instructions added between examples
     add  zero, zero, zero  
@@ -87,6 +94,8 @@ taken:
 # ----------------------------------------------------------------------------------------
 # TODO: Add an example with a load stalling for 1 cycle to pass a value to a NOT-TAKEN branch 
 #  Is this a data hazard or a control hazard?
+    lw   t3, 4(a0)
+    beq  t3, zero, exit
 # ----------------------------------------------------------------------------------------
     # nop instructions added between examples
     add  zero, zero, zero  
@@ -95,6 +104,11 @@ taken:
 
 # ----------------------------------------------------------------------------------------
 # TODO: Add an example with taken branch to a label which is immediately following the branch
+ beq  zero, s0, nextInstr
+nextInstr:
+    add  t0, s1, s2 # How far does this make it to the pipeline? Is is fetched twice?
+    add  t1, s2, s3 # How about this one?
+
 # ----------------------------------------------------------------------------------------
 
 
